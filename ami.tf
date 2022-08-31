935a54d6750ff840d63052907ad97e6d317d7fa0
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-08-30 23:15:59 -0700

    updating ./roboshop-source-code/payment

--- ami.tf
+++ ami.tf
@@ -2,18 +2,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
 terraform {
   backend "s3" {
     bucket = "terraform-kvbucket"
-    key    = "ami/frontend/terraform.tfstate"
+    key    = "ami/payment/terraform.tfstate"
     region = "us-east-1"
   }
 }
 
 module "ami" {
-  source      = "github.com/krishnavamsi7616/immutable-app-ami"
-  COMPONENT   = "payment"
+  source = "github.com/krishnavamsi7616/immutable-app-ami"
+  COMPONENT = "payment"
   APP_VERSION = var.APP_VERSION
 }
 
